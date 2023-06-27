45ecc952ba3ee683bc5c256f75f594453662ea17
Pabitra Kumar Bebartta <pabitrakumarbebartta@gmail.com>
2023-06-27 18:31:23 +0530

    Day-03 done

---
+++ Coding/Day-03/modularExponention.java
@@ -0,0 +1,15 @@
+public class modularExponention {
+    public static void main(String[] args) {
+        System.out.println(modularExponentiation(10000000, 10, 10000005));
+    }
+    public static int findExp(long x,long n,long m){
+        if(n==0)return 1;
+        long sq = findExp(x,n/2,m);
+        int res = (int)(((sq%m) * sq)%m);
+        if(n%2==1)return (int)((res*x)%m);
+        return res;
+    }
+    public static int modularExponentiation(int x, int n, int m) {
+        return findExp( x , n , m);
+    }
+}
