00f0531ed89f69a980817a55a04a4906b7fdb4a3
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2024-01-15 14:16:10 +0530

    some tests

--- Coding/Day-01/KadenAlgorithm.java
+++ Coding/Day-01/KadenAlgorithm.java
@@ -1,17 +1,19 @@
 public class KadenAlgorithm {
     public static void main(String[] args) {
-        int[] arr = {3 , -5 , 4 , 8 , -2, 1};
+        int[] arr = { 3, -5, 4, 8, -2, 7 };
         int n = arr.length;
-        long res = maxSubarraySum(arr , n);
+        long res = maxSubarraySum(arr, n);
         System.out.println(res);
     }
+
     public static long maxSubarraySum(int[] arr, int n) {
-        long maxSum = arr[0] , sum =0 ;
-		for(int i =0 ; i<n ; i++)
-        {
+        long maxSum = arr[0], sum = 0;
+        for (int i = 0; i < n; i++) {
             sum += arr[i];
-            if(sum > maxSum) maxSum = sum;
-            if(sum < 0) sum = 0;
+            if (sum > maxSum)
+                maxSum = sum;
+            if (sum < 0)
+                sum = 0;
         }
         return maxSum;
     }
--- Coding/Day-01/NextPermutation.java
+++ Coding/Day-01/NextPermutation.java
@@ -2,45 +2,40 @@ import java.util.*;
 
 public class NextPermutation {
     public static void main(String[] args) {
-        int[] a = {3,2,1};  // Input
+        int[] a = { 1, 2, 4, 5, 7, 6, 3 }; // Input
         nextPermutation(a);
         System.out.println(Arrays.toString(a));
     }
+
     public static void nextPermutation(int[] nums) {
-        int bg =0 , sm=0 ;
-        for(int i =nums.length-2 ; i>=0 ; i--)
-        {
-            if(nums[i]<nums[i+1])
-            {
-                bg = i+1 ;
+        int bg = 0, sm = 0;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                bg = i + 1;
                 sm = i;
                 break;
             }
         }
-        for(int i =nums.length-1 ; i>=bg ; i--)
-        {
-            if(nums[i]>nums[sm])
-            {
-                swap(nums , i , sm);
+        for (int i = nums.length - 1; i >= bg; i--) {
+            if (nums[i] > nums[sm]) {
+                swap(nums, i, sm);
                 break;
             }
         }
-        reverse(nums , bg);
+        reverse(nums, bg);
     }
 
-    public static void swap (int[] nums , int i , int j )
-    {
-        if(i==j) return;
+    public static void swap(int[] nums, int i, int j) {
+        if (i == j)
+            return;
         nums[i] = nums[i] + nums[j];
         nums[j] = nums[i] - nums[j];
         nums[i] = nums[i] - nums[j];
     }
 
-    public static void reverse(int[] nums , int j)
-    {
-        for(int i =j ; i<=(((nums.length-1-j)/2)+j) ; i++)
-        {
-            swap(nums , i , nums.length-1-(i-j));
+    public static void reverse(int[] nums, int j) {
+        for (int i = j; i <= (((nums.length - 1 - j) / 2) + j); i++) {
+            swap(nums, i, nums.length - 1 - (i - j));
         }
     }
 }
